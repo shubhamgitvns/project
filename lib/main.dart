033d289b37508409c682934faed83688ab9019f1
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-06-10 19:48:27 +0530

    Add success fully liner percent bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,53 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 // import 'package:percent_indicator_example/sample_circular_page.dart';
 // import 'package:percent_indicator_example/sample_linear_page.dart';
+//
+import 'package:flutter/material.dart';
+import 'package:project/stateless.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: SamplePage())));
+  runApp(const MyApp());
 }
 
-class SamplePage extends StatefulWidget {
-  @override
-  _SamplePageState createState() => _SamplePageState();
-}
 
-class _SamplePageState extends State<SamplePage> {
-  void _openPage(Widget page) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (BuildContext context) => page,
-      ),
-    );
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
 
-            CircularPercentIndicator(
-              radius: 60.0,
-              lineWidth: 5.0,
-              percent: 0.5,
-              center: const Text("100%"),
-              progressColor: Colors.green,
-            )
-            ,
-            LinearPercentIndicator(
-              width: 140.0,
-              lineHeight: 14.0,
-              percent: 0.5,
-              backgroundColor: Colors.grey,
-              progressColor: Colors.blue,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+
+
+
+
+
+// void main() {
+//   runApp(MaterialApp(home: Scaffold(body: SamplePage())));
+// }
+//
+// class SamplePage extends StatefulWidget {
+//   @override
+//   _SamplePageState createState() => _SamplePageState();
+// }
+//
+// class _SamplePageState extends State<SamplePage> {
+//   void _openPage(Widget page) {
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(
+//         builder: (BuildContext context) => page,
+//       ),
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Center(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//
+//             CircularPercentIndicator(
+//               radius: 60.0,
+//               lineWidth: 5.0,
+//               percent: 0.5,
+//               center: const Text("100%"),
+//               progressColor: Colors.green,
+//             )
+//             ,
+//             LinearPercentIndicator(
+//               width: 140.0,
+//               lineHeight: 14.0,
+//               percent: 0.5,
+//               backgroundColor: Colors.grey,
+//               progressColor: Colors.blue,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/statefull.dart
+++ lib/statefull.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,6 +11,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  void _openPage(Widget page) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => page,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,19 +59,27 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: const [
-                  Text("INDIVIDUAL",style: TextStyle(color: Colors.white),),
-                  Text("PROFESSIONAL",style: TextStyle(color: Colors.white),),
-                  Text("MERCHANT",style: TextStyle(color: Colors.white),)
+                  Text(
+                    "INDIVIDUAL",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    "PROFESSIONAL",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    "MERCHANT",
+                    style: TextStyle(color: Colors.white),
+                  )
                 ],
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children:[
-                Expanded (
+              children: [
+                Expanded(
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-
                     child: TextField(
                       decoration: InputDecoration(
                         filled: true,
@@ -69,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           borderSide: BorderSide.none,
                           borderRadius: BorderRadius.circular(30),
                         ),
-                        hintText: 'Enter the flex values',
+                        hintText: 'Search',
                       ),
                     ),
                   ),
@@ -80,77 +98,95 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children:  [
+              children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ClipRRect(
                       borderRadius: BorderRadius.circular(20),
                       child: Container(
-                        height: 200,
-
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                         boxShadow: [
-                           BoxShadow(
-                             color: Colors.grey.shade500,
-                             offset: const Offset(4.0,4.0),
-                             blurRadius: 15,
-                             spreadRadius: 1.0,
-                           ),
-                            BoxShadow(
-                             color: Colors.grey.shade800,
-                             offset: const Offset(-4.0,-4.0),
-                             blurRadius: 20,
-                             spreadRadius: 1.0,
-                           )
-                         ]
-                        ),
-                        child:Column(
+                        width: 400,
+                        //height: 200,
+                        decoration:
+                            BoxDecoration(color: Colors.white, boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade500,
+                            offset: const Offset(4.0, 4.0),
+                            blurRadius: 15,
+                            spreadRadius: 1.0,
+                          ),
+                          BoxShadow(
+                            color: Colors.grey.shade800,
+                            offset: const Offset(-4.0, -4.0),
+                            blurRadius: 20,
+                            spreadRadius: 1.0,
+                          )
+                        ]),
+                        child: Column(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-
                             Row(
                               children: const [
                                 Padding(
-                                  padding: EdgeInsets.all(18.0),
-                                  child:  CircleAvatar(
-                                    backgroundImage: AssetImage((("images/w.png"))),
-                                  )
+                                    padding: EdgeInsets.all(18.0),
+                                    child: SizedBox(
+                                      height: 100,
+                                      width: 100,
+                                      child: CircleAvatar(
+                                        backgroundImage:
+                                            AssetImage((("images/w.png"))),
+                                      ),
+                                    )),
+                                Text(
+                                  "Michael Murphy",
+                                  style: TextStyle(
+                                      color: Colors.teal,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                LinearPercentIndicator(
+                                  width: 140.0,
+                                  lineHeight: 14.0,
+                                  percent: 0.5,
+                                  backgroundColor: Colors.grey[50],
+                                  progressColor: Colors.grey[600],
                                 ),
-                                Text("Michael Murphy",style: TextStyle(color: Colors.teal,fontSize: 20,fontWeight: FontWeight.bold),)
                               ],
                             ),
-
-                            Row(
-
-                              children: const [
-                                Text("San Francisco,Within 1 km",style: TextStyle(color: Colors.teal,fontSize: 15),)
-                              ],
-
+                            const Text(
+                              "San Francisco,Within 1 km",
+                              style:
+                                  TextStyle(color: Colors.teal, fontSize: 15),
                             ),
-
-                            Row(
-                              children: const [
-                                Text("Friendship|Coffer|Hangout",style: TextStyle(color: Colors.teal,fontSize: 20,fontWeight: FontWeight.bold),)
-                              ],
+                            const Text(
+                              "Friendship|Coffer|Hangout",
+                              style: TextStyle(
+                                  color: Colors.teal,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
                             ),
-
-                            Row(
-                              children: const [
-                                Text("Hii Community! I am open to new connections",style: TextStyle(color: Colors.teal,fontSize: 15),)
-                              ],
-
-                            ),
+                            const Text(
+                              "Hii Community! I am open to new connections",
+                              style:
+                                  TextStyle(color: Colors.teal, fontSize: 15),
+                            )
                           ],
                         ),
-                      )
-                  ),
+                      )),
                 ),
               ],
             ),
+            
+
+
+
+
           ],
         ));
   }
